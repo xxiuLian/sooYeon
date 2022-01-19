f104a0e0b81f00de5af4389af46c1445e935cdfd
xxiuLian <96617939+xxiuLian@users.noreply.github.com>
2022-01-19 12:59:30 +0900

    static 1 run

--- Run.java
+++ Run.java
@@ -1,32 +1,39 @@
-package com.method.hw2.run;
+package com.method.hw3.run;
 
-import com.method.hw2.controller.NonStaticSample;
+import com.method.hw3.controller.StaticSample;
 
 public class Run {
 
 	public static void main(String[] args) {
-		NonStaticSample ns = new NonStaticSample();
+//		StaticSample ss = new StaticSample();
+//		ss.setValue("java");
+//		System.out.println("value : " + ss.getValue());
+//		
+//		ss.toUpper();
+//		System.out.println("를 대문자로 : " +ss.getValue());
+//		
+//		System.out.println("길이 : " + ss.valueLength());
+//		
+//
+//		System.out.println("Programming 붙이기" + ss.valueConcat("PROGRAMMING"));
+//		
+//		ss.setChar(0, 'C');
+//		System.out.println("J => C : " + ss.getValue());
 		
-		System.out.print("- 크기가 5인 배열의 랜덤값 : ");
-		int[] arr = ns.intArrayAllocation(5); // intArrayAllocation(int length)
-		ns.display(arr); //display(int[] arr)
+		StaticSample.setValue("java");
+		System.out.println("value : " + StaticSample.getValue());
+		
+		StaticSample.toUpper();
+		System.out.println("를 대문자로 : " + StaticSample.getValue());
+		
+		System.out.println("길이 : " + StaticSample.valueLength());
+		
 
-		System.out.println();
-		System.out.print("- 내림차순 출력 : ");
-		ns.sortDescending(arr);
+		System.out.println("Programming 붙이기" + StaticSample.valueConcat("PROGRAMMING"));
 		
-		
-		System.out.println();
-		System.out.print("- 오름차순 출력 : ");
-		ns.sortAscending(arr);
-		
-		
-		System.out.println();
-		System.out.println("\n- apple문자열에 p의 개수 : " + ns.countChar("apple", 'p'));
-		System.out.println("- 13과 7사이 정수들의 합계 : " + ns.totalValue(13, 7));
-		System.out.println("- programming문자열의 3번 인덱스 문자 : " + ns.pCharAt("programming", 3));
-//		System.out.println("- programming문자열의 3번째 문자 : " + ns.pCharAt2("programming", 3));
-		System.out.println("- JAVA와 programming을 합친 문자열 : "  + ns.pConcat("JAVA", "programming"));
+		StaticSample.setChar(0, 'C');
+		System.out.println("J => C : " + StaticSample.getValue());
+
 	}
 
 }
